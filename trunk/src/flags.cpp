f91b1890cd825fb4a8e9f0fbf0b1a72d2ee5d70f
michdrob@gmail.com <michdrob@gmail.com@4981fbb1-9d07-cd24-877e-2d56e44a5676>
2012-12-08 21:17:13 +0000

    -working flag 'h' (line numbers) -added flag 'i' (case insensitivity), it only needs to be passed to Regex object and taken on board Flags can be used in several combinations, no order required

--- trunk/src/flags.cpp
+++ trunk/src/flags.cpp
@@ -1,40 +1,62 @@
 #include "flags.h"
 
+/*
+ * parsing flags
+ * Fabric for RegexFinder object
+ *            FileReader object
+ */
 Flags::Flags(int argc, char** argv){
 
     int opt;
-    frMethod="synchronous";
+    methodChar=0;
+    flagsChar=new char[1]; flagsChar[0]=0;
+    bool flagGiven=false;
+    char flagCaseInsensitivity=0;
 
-    //if 'a' xor 's' flag passed
-    if((opt=getopt(argc,argv,"sa")) !=-1){
-        rf=initRegexFinder(argv[2]);
-
+    /*flag passed
+     *'a' xor 's'
+     *'i' - case insensitive
+     *'h' - show line numbers
+     */
+    opterr=0; //no stderr
+    while((opt=getopt(argc,argv,"saih")) !=-1){
+        if(opt=='s' && methodChar==0){
+            methodChar='s';
+        } else if(opt=='a' && methodChar==0){
+            methodChar='a';
+        }
         switch(opt){
-        case 's':
-            //std::cout<<"SYNC\n"<<std::endl;
-            correctArgs(argc,argv);
-            fr= new SynchronousFileReader(argc, argv, rf);
+        case 'i':
+            flagCaseInsensitivity='i';
             break;
-        case 'a':
-            //std::cout<<"aSYNC\n"<<std::endl;
-            correctArgs(argc,argv);
-            fr= new AsynchronousFileReader(argc, argv, rf);
-            frMethod="asynchronous";
+        case 'h':
+            flagsChar[0]='h';
             break;
         case '?':
-            std::cout << argv[0] <<": Wrong flag. Possible flags: -a -s " << std::endl;
-            //return 1;
+            std::cout<<"There's no flag '-"<<(char)optopt<<"'"<<std::endl;
             break;
         default:
               ;
         }
-    //no flags passed so sync
-    } else {
-        //std::cout<<"brak flag: SYNC\n"<<std::endl;
-        rf=initRegexFinder(argv[1]);
-        fr=new SynchronousFileReader(argc, argv, rf);
+        if(!flagGiven) flagGiven=true;
     }
+    if(flagGiven) correctArgs(argc,argv,optind);
 
+    //creating RefexFinder
+    rf=initRegexFinder(argv[1]);
+
+    //creating proper FileReader
+    switch(methodChar){
+        case 's':
+            fr= new SynchronousFileReader(argc, argv, rf);
+            break;
+        case 'a':
+            fr= new AsynchronousFileReader(argc, argv, rf);
+            break;
+        default:
+        //no flags so SYNC
+            fr= new SynchronousFileReader(argc, argv, rf);
+    }
 }
 
 FileReader* Flags::getFileReader() {
@@ -44,11 +66,12 @@ FileReader* Flags::getFileReader() {
 /*
  * switch argv[i] value with next argv value (argv[i+1])
  * to keep proper regex and files order
+ * optind is the index in argv of the first argv-element that is not an option.
  */
-void Flags::correctArgs(int& argc, char** argv){
+void Flags::correctArgs(int& argc, char** argv,int optind){
     int i;
-    for(i=1;i < argc;++i) argv[i]=argv[i+1];
-    argc--;
+    for(i=1;i < argc;++i) argv[i]=argv[i+optind-1];
+    argc=argc-optind+1;
 }
 
 
@@ -64,4 +87,5 @@ RegexFinder* Flags::initRegexFinder(std::string rgx){
 Flags::~Flags(){
     delete fr;
     delete rf;
+    delete flagsChar;
 }
