9b5548773f62128df709a4e3d0dd843c45f58e7e
adam.syrek@gmail.com <adam.syrek@gmail.com@4981fbb1-9d07-cd24-877e-2d56e44a5676>
2012-12-16 15:53:17 +0000

    Poprawione obliczanie miejsc wystapien w DummyRegexFinder, poprawiony ResultLine (const string& gdzie tylko sie da)

--- src/dummyregexfinder.cpp
+++ src/dummyregexfinder.cpp
@@ -10,10 +10,11 @@ DummyRegexFinder::DummyRegexFinder(char *str) : RegexFinder()
 bool DummyRegexFinder::checkLine(ResultLine &line)
 {
     const char* str = line.getLine().c_str();
+    const char* tmp_str = line.getLine().c_str();
     const char* pos = 0;
     bool found = false;
     while( (pos = strstr(str, rgx)) != NULL) {
-        int tmp = strlen(str) - strlen(pos);
+        int tmp = strlen(tmp_str) - strlen(pos);
         int_pair_t occurence = std::make_pair(tmp, tmp + strlen(rgx));
         line.addOccurence(occurence);
         str = pos + strlen(rgx);
